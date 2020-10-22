a0b5a73321c84a8a0269ad5be1f1812c766816f2
JWHanna <64369647+JWHanna@users.noreply.github.com>
2020-10-22 12:47:19 -0400

    Practice with mapping single points.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -16,8 +16,35 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
            }); 
            - This method is useful when we need to add multiple tile layers, or a background image of our map(s) */
 
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a cirle to the map circle()
+/*L.circle([34.0522, -118.2437], {
+  radius: 100
+}).addTo(map);*/
+
+// SKILL DRILL 13.4.1 circle()
+/*L.circle([34.0522, -118.2437], {
+  color: 'black',
+  fillColor:'#ffffa1',
+  radius: 300
+}).addTo(map)*/
+
+
+// Add a cirlce to the map circleMarker()
+//L.circleMarker([34.0522, -118.2437]).addTo(map);
+// - Default radius is 10 pixels
+
+// SKILL DRILL 13.4.1 circleMarker()
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: 'black',
+  fillColor: '#ffffa1'
+}).addTo(map);
+
 // Add tile layer to Map for background
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
