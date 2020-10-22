9dd3951ae390fc3b8c1edcdb2cb1518016305a52
JWHanna <64369647+JWHanna@users.noreply.github.com>
2020-10-22 16:37:06 -0400

    Cleaning some unnecessary comments and notes.

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -3,18 +3,6 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
-// - Assigning the variable map to the object L.map(), instantiate the object with the given string 'mapid'
-// - mapid will reference the id tag in our <div> element on the index.html file
-/* - setView() method sets the view of the map with a geographical center, where the first coordinate is 
-     latitude (40.7) and the second is longitude (-94.5). Zoom level is set to “4” on a scale 0–18. */
-     /* - Alternative to setView():
-          let map = L.map("mapid", {
-            center: [
-                40.7, -94.5
-            ],
-            zoom: 4
-           }); 
-           - This method is useful when we need to add multiple tile layers, or a background image of our map(s) */
 
 // Get data from cities.js
 let cityData = cities;
